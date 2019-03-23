4b48e891325a395c1a19c28af8e2b242907b3c4b
realsongtao <realsongtao@163.com>
2019-03-23 23:55:42 +0800

    edit hotfix

--- src/main/java/com/jt/git/Hello.java
+++ src/main/java/com/jt/git/Hello.java
@@ -1,17 +1,10 @@
 package com.jt.git;
 
 public class Hello {
-<<<<<<< HEAD
 	public static void main(String[] args) {
-		System.out.println("right");
+		System.out.println("hotfix");
 	}
 	public void sst() {
-		System.out.println("RRR");
-=======
-	@SuppressWarnings("unused")
-	private void syso() {
-		System.out.println("left");
-
->>>>>>> branch 'master' of https://github.com/realsongtao/testgit.git
+		System.out.println("hotfix");
 	}
 }
