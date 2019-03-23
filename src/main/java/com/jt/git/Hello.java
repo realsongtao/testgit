bdccceffbd0b7ba285e1a6ad95716810c8e5473c
realsongtao <realsongtao@163.com>
2019-03-24 00:04:25 +0800

    hot2

--- src/main/java/com/jt/git/Hello.java
+++ src/main/java/com/jt/git/Hello.java
@@ -2,9 +2,9 @@ package com.jt.git;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("hotfix");
+		System.out.println("hot2");
 	}
 	public void sst() {
-		System.out.println("hotfix");
+		System.out.println("hot2");
 	}
 }
---
+++ src/main/java/com/jt/git/Hot2.java
@@ -0,0 +1,7 @@
+package com.jt.git;
+
+public class Hot2 {
+public static void main(String[] args) {
+	System.out.println("hot2");
+}
+}
